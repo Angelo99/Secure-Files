9c55535ab41fc27adb95869fa9a85ea65e14f014
h3llwings <h3llwings@gmail.com>
2017-04-04 21:35:59 +0530

    upload from desktop
    sec-uploads

---
+++ AESFileEncryptor/AESFileEncryptor/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace AESFileEncryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnselect = new System.Windows.Forms.Button();
+            this.txtpath = new System.Windows.Forms.TextBox();
+            this.btnen = new System.Windows.Forms.Button();
+            this.btnde = new System.Windows.Forms.Button();
+            this.openFileDia = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPasswrd = new System.Windows.Forms.TextBox();
+            this.txtPasswordConfirm = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnselect
+            // 
+            this.btnselect.Location = new System.Drawing.Point(403, 199);
+            this.btnselect.Name = "btnselect";
+            this.btnselect.Size = new System.Drawing.Size(99, 23);
+            this.btnselect.TabIndex = 0;
+            this.btnselect.Text = "Select File";
+            this.btnselect.UseVisualStyleBackColor = true;
+            this.btnselect.Click += new System.EventHandler(this.btnselect_Click);
+            // 
+            // txtpath
+            // 
+            this.txtpath.Location = new System.Drawing.Point(60, 201);
+            this.txtpath.Name = "txtpath";
+            this.txtpath.ReadOnly = true;
+            this.txtpath.Size = new System.Drawing.Size(268, 20);
+            this.txtpath.TabIndex = 1;
+            this.txtpath.Visible = false;
+            // 
+            // btnen
+            // 
+            this.btnen.Enabled = false;
+            this.btnen.Location = new System.Drawing.Point(403, 229);
+            this.btnen.Name = "btnen";
+            this.btnen.Size = new System.Drawing.Size(205, 23);
+            this.btnen.TabIndex = 2;
+            this.btnen.Text = "Encrypt";
+            this.btnen.UseVisualStyleBackColor = true;
+            this.btnen.Click += new System.EventHandler(this.btnen_Click);
+            // 
+            // btnde
+            // 
+            this.btnde.Enabled = false;
+            this.btnde.Location = new System.Drawing.Point(403, 261);
+            this.btnde.Name = "btnde";
+            this.btnde.Size = new System.Drawing.Size(205, 23);
+            this.btnde.TabIndex = 3;
+            this.btnde.Text = "Decrypt";
+            this.btnde.UseVisualStyleBackColor = true;
+            this.btnde.Click += new System.EventHandler(this.btnde_Click);
+            // 
+            // openFileDia
+            // 
+            this.openFileDia.Filter = "All files (*.*)|*.*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Path";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Confirm Password";
+            // 
+            // txtPasswrd
+            // 
+            this.txtPasswrd.Location = new System.Drawing.Point(93, 21);
+            this.txtPasswrd.Name = "txtPasswrd";
+            this.txtPasswrd.PasswordChar = '*';
+            this.txtPasswrd.Size = new System.Drawing.Size(199, 20);
+            this.txtPasswrd.TabIndex = 7;
+            // 
+            // txtPasswordConfirm
+            // 
+            this.txtPasswordConfirm.Location = new System.Drawing.Point(93, 47);
+            this.txtPasswordConfirm.Name = "txtPasswordConfirm";
+            this.txtPasswordConfirm.PasswordChar = '*';
+            this.txtPasswordConfirm.Size = new System.Drawing.Size(199, 20);
+            this.txtPasswordConfirm.TabIndex = 8;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader4,
+            this.columnHeader3});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(1, 1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(613, 194);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Path";
+            this.columnHeader1.Width = 209;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "File type";
+            this.columnHeader2.Width = 168;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Size(MB)";
+            this.columnHeader4.Width = 93;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            this.columnHeader3.Width = 152;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.removeToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.clearAllToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(119, 54);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.removeToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(115, 6);
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.clearAllToolStripMenuItem.Text = "Clear All";
+            this.clearAllToolStripMenuItem.Click += new System.EventHandler(this.clearAllToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Select Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(298, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Gen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtPasswrd);
+            this.groupBox1.Controls.Add(this.txtPasswordConfirm);
+            this.groupBox1.Location = new System.Drawing.Point(9, 198);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(388, 86);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(568, 298);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(35, 13);
+            this.linkLabel1.TabIndex = 14;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "About";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(6, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "SLIIT Cyber Sec SSS Assignment 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 316);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnde);
+            this.Controls.Add(this.btnen);
+            this.Controls.Add(this.txtpath);
+            this.Controls.Add(this.btnselect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Secure Files";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnselect;
+        private System.Windows.Forms.TextBox txtpath;
+        private System.Windows.Forms.Button btnen;
+        private System.Windows.Forms.Button btnde;
+        private System.Windows.Forms.OpenFileDialog openFileDia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPasswrd;
+        private System.Windows.Forms.TextBox txtPasswordConfirm;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AESFileEncryptor/AESFileEncryptor/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+ 
+ namespace AESFileEncryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Crypto.Crypto CryptoFunctions = new Crypto.Crypto();
+        private void btnen_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                int ii = 0;
+                
+                try
+                {
+                    if (listView1.Items[i].SubItems[1].Text == ".enc") //check if file type is alredy encrypted,if true popup error and skip that file
+                    {
+                        
+                        listView1.Items[i].SubItems[3].Text = "Error";
+                        listView1.Items[i].BackColor = Color.OrangeRed;
+                    }
+                    else
+                    {
+
+                        if(txtPasswrd.Text!="" && txtPasswordConfirm.Text != "")//check if password is not empty
+                        {
+                            if (txtPasswrd.Text == txtPasswordConfirm.Text)//check both passwords are equels
+                            {
+
+                                File.Copy(listView1.Items[i].SubItems[0].Text, listView1.Items[i].SubItems[0].Text + ".enc"); //get copy of original file and save it to save location with .enc extention
+
+                                byte[] bytesToBeEncrypted = File.ReadAllBytes(listView1.Items[i].SubItems[0].Text + ".enc");//read all bytes of file and save it to byte array
+                                byte[] passwordBytes = Encoding.UTF8.GetBytes(txtPasswrd.Text);//get password from txtbox
+
+                                // Hash the password with SHA256
+                                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                                byte[] bytesEncrypted = CryptoFunctions.AES_Encrypt(bytesToBeEncrypted, passwordBytes); //encrypt bytes with password
+
+                                string fileEncrypted = listView1.Items[i].SubItems[0].Text + ".enc";
+
+                                File.WriteAllBytes(fileEncrypted, bytesEncrypted); //write encrypted byte array to disk
+                                listView1.Items[i].SubItems[3].Text = "Encrypted";
+                                listView1.Items[i].BackColor = Color.LimeGreen;
+                            }
+                            else
+                                MessageBox.Show("Password does not match the confirm password");
+                        }
+                        else
+                            MessageBox.Show("Enter Password");
+
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+              
+            }
+
+
+            txtPasswordConfirm.Clear(); txtPasswrd.Clear();
+        }
+
+        private void btnde_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                int ii = 0;
+                if (listView1.Items[i].SubItems[1].Text == ".enc") //check the file is ends with .enc extetion decrypt.if not skip that file
+                {
+                    try
+                    {
+                        if (txtPasswrd.Text != "" && txtPasswordConfirm.Text != "") //check if password is not empty
+                        {
+                            if (txtPasswrd.Text == txtPasswordConfirm.Text)//check both passwords are equels
+                            {
+                                byte[] bytesToBeDecrypted = File.ReadAllBytes(listView1.Items[i].SubItems[0].Text);//read all bytes of file and save it to byte array
+                                byte[] passwordBytes = Encoding.UTF8.GetBytes(txtPasswrd.Text);
+                                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                                byte[] bytesDecrypted = CryptoFunctions.AES_Decrypt(bytesToBeDecrypted, passwordBytes);//decrypt encrypted bytearray
+
+                               
+                                string file = listView1.Items[i].SubItems[0].Text.Remove(listView1.Items[i].SubItems[0].Text.Length - 3);
+
+                                File.WriteAllBytes(file, bytesDecrypted);//save decrypted file into disk
+
+                                listView1.Items[i].SubItems[3].Text = "Decrypted";
+                                listView1.Items[i].BackColor = Color.GreenYellow;
+                            }
+                            else
+                                MessageBox.Show("Password does not match the confirm password");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Enter Password to Decrypt");
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+                ii++;
+            }
+            txtPasswordConfirm.Clear();txtPasswrd.Clear();
+      
+        }
+
+        private void btnselect_Click(object sender, EventArgs e)
+        {
+          
+            DialogResult result = openFileDia.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FileInfo FI = new FileInfo(openFileDia.FileName);
+                 float size = (FI.Length / 1024f) / 1024f; //get size of file
+                ListViewItem item1 = new ListViewItem(openFileDia.FileName);
+                txtpath.Text = openFileDia.FileName;
+                item1.SubItems.Add(FI.Extension); //get extetion of a file
+                item1.SubItems.Add( size.ToString());
+                item1.SubItems.Add("idle");
+                listView1.Items.Add(item1);
+
+                btnen.Enabled = true;
+                btnde.Enabled = true;
+            }
+                
+
+         
+          
+        }
+
+        public static string GetRandomString()
+        {
+            string path = Path.GetRandomFileName();
+            path = path.Replace(".", ""); // Remove period.
+            return path;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Convert Password textbox to Normal txtbox
+            txtPasswrd.PasswordChar = '\0';
+            txtPasswordConfirm.PasswordChar = '\0';
+
+            txtPasswrd.Text = GetRandomString();
+            txtPasswordConfirm.Text = txtPasswrd.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string foldername = folderBrowserDialog1.SelectedPath;
+                foreach (string f in Directory.GetFiles(foldername))
+                {
+                    FileInfo FI = new FileInfo(f);
+                    
+                    
+                    float size = (FI.Length / 1024f) / 1024f;
+                    ListViewItem item1 = new ListViewItem(f);
+                   
+                    item1.SubItems.Add(FI.Extension);
+                    item1.SubItems.Add(size.ToString());
+                    item1.SubItems.Add("idle");
+                    listView1.Items.Add(item1);
+                    btnen.Enabled = true;
+                    btnde.Enabled = true;
+                }
+            }
+        }
+
+        private void removeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //remove selected item from context menu
+            DialogResult Ds;
+            Ds=MessageBox.Show("Do you want to remove "+ listView1.SelectedItems[0].Text +" ?","remove alert",MessageBoxButtons.YesNo);
+            if(Ds==DialogResult.Yes)
+                listView1.SelectedItems[0].Remove();
+            //MessageBox.Show(listView1.SelectedItems[0].Text); 
+        }
+
+        private void clearAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://h3llwings.wordpress.com/");
+            MessageBox.Show("Angelo Ruwantha");
+        }
+    }
+}
